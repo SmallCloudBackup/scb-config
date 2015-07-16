75e3984f5f46b581ee8a79a275e26e198c91fb4b
Valentin Alexeev <valentin.alekseev@gmail.com>
2015-07-16 12:33:43 +0300

    Move scb-config to its own repository

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * SmallCloudBackup - Application configuration interface
+ *
+ * This web application provides interface to configure SCB:
+ * - sources
+ * - transformation and versioning
+ * - outputs
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var routes = require('./routes/index');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var oneDay = 86400000;
+
+app.use(express.static(path.join(__dirname, 'bower_components'), { maxAge: oneDay }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneDay }));
+
+app.use('/', routes);
+
+module.exports = app;

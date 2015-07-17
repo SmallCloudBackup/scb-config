64aa9baf3ca5fe0cb35b0a252ead1cdcac295d1c
Valentin Alexeev <valentin.alekseev@gmail.com>
2015-07-18 01:23:38 +0300

    Major: new REST APIs

--- routes/api.js
+++ routes/api.js
@@ -4,22 +4,41 @@ var async = require('async');
 var core = require('scb-core');
 
 /** Get list of defined services. */
-router.get('/services', function(req, res, next) {
+router.get(/^\/(services|sources|sinks|notifications|jobs|tasks|transformations)/, function(req, res, next) {
   async.waterfall([
-    function requestServices (next) {
-        core.core.listServices(next);
+    function request (next) {
+      // XXX a small hack thanks to english language - we just need to remove trailing S to get object type
+      var type = req.params[0];
+      core.core.list(type.substring(0, type.length - 1), next);
     },
     function sendResult(listOfServices, next) {
-      console.log(arguments);
       if (listOfServices != null) {
         res.json({
           ok: true,
-          services: listOfServices
+          objects: listOfServices
         });
         next();
       }
     }
   ], next);
 });
+
+router.get(/^\/(service|source|sink|notification|job|task|transformation)\/(\w+)/, function (req, res, next) {
+  console.log("type=" + req.params[0] + " id=" + req.params[1]);
+  async.waterfall([
+    function requestObject (next) {
+        core.core.getObjectById(req.params[0], req.params[1], next);
+    },
+    function sendResult(obj, next) {
+      if (obj != null) {
+        res.json({
+          ok: true,
+          object: obj
+        });
+        next();
+      }
+    }
+  ], next);
+})
 
 module.exports = router;
